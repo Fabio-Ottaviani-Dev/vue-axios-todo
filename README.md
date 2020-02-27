bebf9e224d287351397a1ad6168c23ddf8d051c9
Fabio-Ottaviani-Dev <F0Dev>
2020-02-27 15:44:20 -0800

    Start, basic implementation.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Vue, Axios, Todos
+A besic REST CRUD Todos App, made with: Vue & Axios
+
+## Dependencies
+
+1. [Vue](https://vuejs.org/)
+2. [Axios](https://github.com/axios/axios)
+3. [lite-server](https://www.npmjs.com/package/lite-server)
+4. [JSON Server](https://github.com/typicode/json-server)
+
+## Setup
+
+``` bash
+
+# 1. Clone the project
+# 2 cd into it
+# 3 install the dependencies
+npm install
+# OR
+npm install lite-server --save-dev
+npm install json-server
+
+```
+
+## Run it
+
+``` bash
+# 1. Run lite-server
+npx lite-server
+# 2. Open another tab in the terminal (ctrl + shift + t)
+# 3. Run json-server
+json-server --port 5000 --watch db.json
+
+```
+
+## How to test json-server endpoint with curl
+
+``` bash
+# Create
+curl -X POST -H "Content-Type: application/json" -d '{"name":"New todo stuff"}' http://localhost:5000/todos
+
+# Read >> All
+curl -X GET http://localhost:5000/todos
+
+# Read >> id 1
+curl -X GET http://localhost:5000/todos/1
+
+# Update >> id 1
+curl -X PATCH -H "Content-Type: application/json" -d '{"name":"Very New todo stuff"}' http://localhost:5000/todos/1
+
+# Delete >> id 1
+curl -X DELETE http://localhost:5000/todos/1
+```
---
+++ main.js
@@ -0,0 +1,83 @@
+const API_URL = "http://localhost:5000"
+
+var app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      todos: [],
+      todoId: null,
+      todoName: '',
+      update: false,
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+  methods: {
+ // Create >> Todo
+    createTodo(){
+      console.log('createTodo');
+      axios.post(`${API_URL}/todos`, {name: this.todoName})
+        .then(res => {
+          // this.todos = [...this.todos, res.data];
+          this.todoName = '';
+          this.readTodos();
+        }).catch(err => {console.log(err);}
+      );
+    },
+  // Read >> All Todos
+    readTodos(){
+      console.log(`readTodo{S}`);
+      axios.get(`${API_URL}/todos`)
+        .then(res => {
+          this.todos = res.data;
+        }).catch(err => {console.log(err);}
+      );
+    },
+  // Update >> Todo
+    editTodo(id = null){
+      if (id != null) {
+        console.log(`editTodo -> id: ${id}`);
+        axios.get(`${API_URL}/todos/${id}`)
+          .then(res => {
+            this.todoId = id;
+            this.todoName = res.data.name;
+            this.update = true;
+          }).catch(err => {console.log(err);}
+        );
+      }
+    },
+  // ---
+    updateTodo(){
+       console.log(`updateTodo patch`);
+       var id = this.todoId;
+       axios.patch(`${API_URL}/todos/${id}`,
+         {name: this.todoName})
+         .then(res => { //console.log(res);
+           this.update = false;
+           this.todoName = "";
+           this.todoId = null
+           this.readTodos();
+         }).catch(err => {console.error(err);}
+       );
+    },
+  // Delete >> Todo
+    deleteTodo(id){
+      console.log(`deleteTodo -> id: ${id}`);
+      axios.delete(`${API_URL}/todos/${id}`)
+        .then(res => { //console.log(res);
+          this.todoName = "";
+          this.update = false;
+          this.readTodos();
+        }).catch(err => {console.error(err);}
+      );
+    },
+  }, // END methods
+  created(){
+     this.readTodos()
+  },
+})
