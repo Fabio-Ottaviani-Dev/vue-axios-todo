5ced0a08e037b025dd69e7b686378e7f04928d81
Fabio-Ottaviani-Dev <F0Dev>
2020-10-08 11:25:03 -0700

    Update Bootstrap 4.5.2, add prettierrc.js file roles, npm packages update.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000"
+const API_URL = 'http://localhost:5000'
 
 var app = new Vue({
   el: '#app',
@@ -14,70 +14,86 @@ var app = new Vue({
     focus: {
       inserted: function (el) {
         el.focus()
-      }
-    }
+      },
+    },
   },
   methods: {
- // Create >> Todo
-    createTodo(){
-      console.log('createTodo');
-      axios.post(`${API_URL}/todos`, {name: this.todoName})
-        .then(res => {
+    // Create >> Todo
+    createTodo() {
+      console.log('createTodo')
+      axios
+        .post(`${API_URL}/todos`, { name: this.todoName })
+        .then((res) => {
           // this.todos = [...this.todos, res.data];
-          this.todoName = '';
-          this.readTodos();
-        }).catch(err => {console.log(err);}
-      );
+          this.todoName = ''
+          this.readTodos()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     },
-  // Read >> All Todos
-    readTodos(){
-      console.log(`readTodo{S}`);
-      axios.get(`${API_URL}/todos`)
-        .then(res => {
-          this.todos = res.data;
-        }).catch(err => {console.log(err);}
-      );
+    // Read >> All Todos
+    readTodos() {
+      console.log(`readTodo{S}`)
+      axios
+        .get(`${API_URL}/todos`)
+        .then((res) => {
+          this.todos = res.data
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     },
-  // Update >> Todo
-    editTodo(id = null){
+    // Update >> Todo
+    editTodo(id = null) {
       if (id != null) {
-        console.log(`editTodo -> id: ${id}`);
-        axios.get(`${API_URL}/todos/${id}`)
-          .then(res => {
-            this.todoId = id;
-            this.todoName = res.data.name;
-            this.update = true;
-          }).catch(err => {console.log(err);}
-        );
+        console.log(`editTodo -> id: ${id}`)
+        axios
+          .get(`${API_URL}/todos/${id}`)
+          .then((res) => {
+            this.todoId = id
+            this.todoName = res.data.name
+            this.update = true
+          })
+          .catch((err) => {
+            console.log(err)
+          })
       }
     },
-  // ---
-    updateTodo(){
-       console.log(`updateTodo patch`);
-       var id = this.todoId;
-       axios.patch(`${API_URL}/todos/${id}`,
-         {name: this.todoName})
-         .then(res => { //console.log(res);
-           this.update = false;
-           this.todoName = "";
-           this.todoId = null
-           this.readTodos();
-         }).catch(err => {console.error(err);}
-       );
+    // ---
+    updateTodo() {
+      console.log(`updateTodo patch`)
+      var id = this.todoId
+      axios
+        .patch(`${API_URL}/todos/${id}`, { name: this.todoName })
+        .then((res) => {
+          //console.log(res);
+          this.update = false
+          this.todoName = ''
+          this.todoId = null
+          this.readTodos()
+        })
+        .catch((err) => {
+          console.error(err)
+        })
     },
-  // Delete >> Todo
-    deleteTodo(id){
-      console.log(`deleteTodo -> id: ${id}`);
-      axios.delete(`${API_URL}/todos/${id}`)
-        .then(res => { //console.log(res);
-          this.todoName = "";
-          this.update = false;
-          this.readTodos();
-        }).catch(err => {console.error(err);}
-      );
+    // Delete >> Todo
+    deleteTodo(id) {
+      console.log(`deleteTodo -> id: ${id}`)
+      axios
+        .delete(`${API_URL}/todos/${id}`)
+        .then((res) => {
+          //console.log(res);
+          this.todoName = ''
+          this.update = false
+          this.readTodos()
+        })
+        .catch((err) => {
+          console.error(err)
+        })
     },
   }, // END methods
-  created(){
-     this.readTodos()
+  created() {
+    this.readTodos()
   },
 })
